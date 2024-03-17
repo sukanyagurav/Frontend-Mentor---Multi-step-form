b8f20708adcf56471a980d3990d88b69903783b3
sukanya gurav <sukanyagurav6@gmail.com>
2024-03-17 12:27:26 +0530

    completed

--- app.js
+++ app.js
@@ -10,15 +10,21 @@ const phone_number = document.getElementById('phone_number') || 0
 const userName = document.getElementById('name') 
 const email = document.getElementById('email') 
 
+let currentId = +(document.querySelector('.show__content').getAttribute('id'))
 
 let selectedPlans =  document.querySelectorAll('.addon input')
-// let addons=[]
+
+const selectPlan = document.getElementById("select")
+const yearly = document.querySelector('.yearly');
+
+var btn = document.querySelector('.change')
+let totalAddon = 0
 
 // stepper
 const allSteps = document.querySelectorAll('.step')
 const steps = document.querySelector('.steps')
 
-
+// store the values
 let details ={
     'personalInfo':{
         name:'',
@@ -84,11 +90,9 @@ function phoneNumberFormat(value){
         val = `+${value[0]} ${value.slice(1,4)} ${value.slice(4,7)} ${value.slice(7,)}`
         details.personalInfo.phoneNumber = val
     }
-    
     return `${val}`
 }
-let currentId = +(document.querySelector('.show__content').getAttribute('id'))
-let temp = currentId
+
 function checkPersonalInfo(active__content){
     const form = active__content.querySelector('form')
     const errormessage= document.querySelector('.error__message')
@@ -104,29 +108,34 @@ function checkPersonalInfo(active__content){
     }
 
 }
-const selectPlan = document.getElementById("select")
-const yearly = document.querySelector('.yearly');
+function clearElement(element){
+    while(element.firstChild){
+        element.removeChild(element.firstChild)
+    }
+}
+
 function updatePlan(){
+   
     if(!selectPlan.checked){
-        yearly.parentElement.style.color='hsl(213, 96%, 18%)'
+
+        yearly.style.color='hsl(213, 96%, 18%)'
+      
         yearly.parentElement.classList.add('showYearly')
         details.plan.planType=yearly.innerHTML
         details.plan.shortPlanName='yr'
         details.plan.planValue=document.querySelector('.active').querySelector('.yearly__plan').querySelector('.plan__value').innerHTML
     }
     else{
+      
         yearly.parentElement.classList.remove('showYearly')
-        yearly.parentElement.style.color='hsl(231, 11%, 63%)'
+        yearly.style.color='hsl(231, 11%, 63%)'
         details.plan.planType= document.querySelector('.month').innerHTML
         details.plan.shortPlanName= 'mo'
         details.plan.planValue=document.querySelector('.active').querySelector('.monthly__plan').querySelector('.plan__value').innerHTML
     }
 
 }
-selectPlan.addEventListener('change',e=>{
-   e.preventDefault()
-   updatePlan()
-})
+
 // EVENT LISTENERS
 userName.addEventListener('input',(e)=>{checkEmpty(e.target)})
 phone_number.addEventListener('input',(e)=>{
@@ -134,47 +143,34 @@ phone_number.addEventListener('input',(e)=>{
     phone_number.value = phoneNumberFormat((phone_number.value))
     checkPhoneLength()
 })
+
 email.addEventListener('input',(e)=>
 {           
             checkEmpty(e.target) 
             checkEmail(e.target)
 })
 
+selectPlan.addEventListener('change',e=>{
+    e.preventDefault()
+  
+    updatePlan()
+ })
 
-function nextContent(active){
-    allArticles.forEach(article=>{
-        if(article.classList.contains('show__content')){
-            article.classList.remove('show__content')
-            article.classList.add('hide__content')
+selectPlan.addEventListener('keydown',e=>{
+    if (e.key === ' ') {
+        
+        e.preventDefault();
+        if(selectPlan.checked){
+            selectPlan.checked=false
+        }else{
+            selectPlan.checked=true
         }
-    })
-    active.nextElementSibling.classList.remove('hide__content')
-    active.nextElementSibling.classList.add('show__content')
-    active__content = active.nextElementSibling
-    currentId = +(active__content.getAttribute('id'))
-    if(currentId > 1){
-        const btn = document.querySelector('.prev-step')
-        btn.classList.add('show__button')
-        btn.classList.remove('hide__button')
-    }else{
-        const btn = document.querySelector('.prev-step')
-        btn.classList.remove('show__button')
-        btn.classList.add('hide__button')
+        updatePlan()
+      
     }
-    if(currentId ==4){
-        next_Btn.innerHTML = 'Confirm'
-        next_Btn.style.backgroundColor=`hsl(243, 100%, 62%)`
-    }
-    if(currentId==5){
-        const btn = document.querySelector('.prev-step')
-        btn.classList.remove('show__button')
-        btn.classList.add('hide__button')
-        next_Btn.classList.remove('show__button')
-        next_Btn.classList.add('hide__button')
-    }
-    currentState(currentId)
-}
-
+    
+ 
+})
 function checkPlan(){
     const allPlan = document.querySelectorAll('.plan')
     allPlan.forEach(plan=>{
@@ -221,8 +217,7 @@ selectedPlans.forEach(plan=>{
         }
     })
 })
-var btn = document.querySelector('.change')
-let totalAddon = 0
+
 function createAddonPlan(){
    const div = document.querySelector('.addon__plan')
    clearElement(div)
@@ -247,11 +242,7 @@ function createAddonPlan(){
    div.append(ul)
    return div
 }
-function clearElement(element){
-    while(element.firstChild){
-        element.removeChild(element.firstChild)
-    }
-}
+
 function result(){
     const basicPlanName = document.querySelector('.basic__plan h2')
     basicPlanName.innerHTML = `${details.plan.planName} <span class="final__plan">(${details.plan.planType})</span>`
@@ -261,19 +252,15 @@ function result(){
     const plan__durationText = document.querySelector('.plan__durationText')
     plan__durationText.innerHTML=details.plan.planType.slice(0,details.plan.planType.length-2).toLowerCase()
     const total = document.querySelector('.total__value')
-
-
     if(details.addOns.length > 0){
-       
-      
         planContainer.append(createAddonPlan())
         total.innerHTML=`$${+details.plan.planValue + totalAddon} / ${details.plan.shortPlanName}`
     }else{
         document.querySelector('.addon__plan').style.display='none' 
         total.innerHTML=`$${details.plan.planValue} / ${details.plan.shortPlanName}`
     }
-
 }  
+// change to second page 
 btn.addEventListener('click',function(e){
    currentId=2
    currentState(currentId)
@@ -284,13 +271,12 @@ btn.addEventListener('click',function(e){
     if(article.classList.contains('show__content')){
         article.classList.remove('show__content')
         article.classList.add('hide__content')
-       active__content= document.getElementById(currentId)
-       active__content.classList.remove('hide__content')
-       active__content.classList.add('show__content')
-     
+        active__content= document.getElementById(currentId)
+        active__content.classList.remove('hide__content')
+        active__content.classList.add('show__content')
     }
-    })
- stepper()
+  })
+   stepper()
 })
 function currentState(currentId){
     
@@ -312,6 +298,20 @@ function currentState(currentId){
     }
 }
 
+
+// next and previous functionality
+function stepper(){
+    allSteps.forEach(step=>{
+        if(step.classList.contains('active__form')){
+            step.classList.remove('active__form')
+        }
+    })
+    if(currentId < 5){
+        steps.querySelector(`.steps .step:nth-child(${currentId})`).classList.add('active__form')
+    }else{
+        steps.querySelector(`.steps .step:nth-child(${currentId-1})`).classList.add('active__form')
+    }
+}
 next_Btn.addEventListener('click',function(e){
     if(currentId == 1){
         currentState(currentId)
@@ -325,22 +325,6 @@ next_Btn.addEventListener('click',function(e){
     }
     stepper()
 })
-
-
-
-
-function stepper(){
-    allSteps.forEach(step=>{
-        if(step.classList.contains('active__form')){
-            step.classList.remove('active__form')
-        }
-    })
-    if(currentId < 5){
-        steps.querySelector(`.steps .step:nth-child(${currentId})`).classList.add('active__form')
-    }else{
-        steps.querySelector(`.steps .step:nth-child(${currentId-1})`).classList.add('active__form')
-    }
-}
 
 previous_Btn.addEventListener('click',function(e){
     allArticles.forEach(article=>{
@@ -365,3 +349,36 @@ previous_Btn.addEventListener('click',function(e){
     stepper()
 })
 
+function nextContent(active){
+    allArticles.forEach(article=>{
+        if(article.classList.contains('show__content')){
+            article.classList.remove('show__content')
+            article.classList.add('hide__content')
+        }
+    })
+    active.nextElementSibling.classList.remove('hide__content')
+    active.nextElementSibling.classList.add('show__content')
+    active__content = active.nextElementSibling
+    currentId = +(active__content.getAttribute('id'))
+    if(currentId > 1){
+        const btn = document.querySelector('.prev-step')
+        btn.classList.add('show__button')
+        btn.classList.remove('hide__button')
+    }else{
+        const btn = document.querySelector('.prev-step')
+        btn.classList.remove('show__button')
+        btn.classList.add('hide__button')
+    }
+    if(currentId ==4){
+        next_Btn.innerHTML = 'Confirm'
+        next_Btn.style.backgroundColor=`hsl(243, 100%, 62%)`
+    }
+    if(currentId==5){
+        const btn = document.querySelector('.prev-step')
+        btn.classList.remove('show__button')
+        btn.classList.add('hide__button')
+        next_Btn.classList.remove('show__button')
+        next_Btn.classList.add('hide__button')
+    }
+    currentState(currentId)
+}
